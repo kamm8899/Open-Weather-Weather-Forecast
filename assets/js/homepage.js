e3e521e750d38da99d0538f6161b8ac5a1a392a2
Jessica Kamman <jessicakammann@Jessicas-MacBook-Air.local>
2021-07-23 15:41:57 -0700

    added style.css

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -1,5 +1,4 @@
-//GIVEN a weather dashboard with form inputs
-//WHEN I search for a city
+
 //THEN I am presented with current and future conditions for that city and that city is added to the search history
 //WHEN I view current weather conditions for that city
 //THEN I am presented with the city name, the date, an icon representation of weather conditions, the temperature, the humidity, the wind speed, and the UV index
@@ -9,3 +8,28 @@
 //THEN I am presented with a 5-day forecast that displays the date, an icon representation of weather conditions, the temperature, the wind speed, and the humidity
 //WHEN I click on a city in the search history
 //THEN I am again presented with current and future conditions for that city
+
+
+//GIVEN a weather dashboard with form inputs
+//WHEN I search for a city
+
+
+var APIKey ='eec3413a16d43f5e64f5215a7760f24b';
+var cityName;
+var temp;
+var humidity;
+var windSpeed;
+var uvIndex;
+
+//get fetch for api
+var getWeatherInfo = function(){
+    fetch("https://api.openweathermap.org/data/2.5/weather?q=Clinton&appid=eec3413a16d43f5e64f5215a7760f24b")
+
+    .then(function(response) {
+        return response.json();
+      })
+      .then(function(response) {
+          console.log(response);
+});
+}
+getWeatherInfo();
