eebd60eda2b65999984e11915ac63e947d6c45de
Jessica Kamman <jessicakammann@Jessicas-MacBook-Air.local>
2021-07-24 09:04:10 -0700

    added functionality for Uv index, and displayWeather Data

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -1,17 +1,17 @@
 
 //THEN I am presented with current and future conditions for that city and that city is added to the search history
 //WHEN I view current weather conditions for that city
+
+
 //THEN I am presented with the city name, the date, an icon representation of weather conditions, the temperature, the humidity, the wind speed, and the UV index
-//WHEN I view the UV index
-//THEN I am presented with a color that indicates whether the conditions are favorable, moderate, or severe
+
 //WHEN I view future weather conditions for that city
 //THEN I am presented with a 5-day forecast that displays the date, an icon representation of weather conditions, the temperature, the wind speed, and the humidity
 //WHEN I click on a city in the search history
 //THEN I am again presented with current and future conditions for that city
 
 
-//GIVEN a weather dashboard with form inputs
-//WHEN I search for a city
+
 
 
 var APIKey ='eec3413a16d43f5e64f5215a7760f24b';
@@ -27,39 +27,75 @@ var cityContainerEl = document.querySelector("#city-container");
 
 
 
+
 //get fetch for api
-var getWeatherInfo = function(city){
+var getWeatherInfo = function(cityName){
     //fetch request to get Munich as a city
     fetch("http://api.openweathermap.org/data/2.5/weather?q="
-        + city
+        + cityName
         + "&appid="
         + APIKey
-        //+ "&units=imperial"
+        + "&units=imperial"
     )
 
     .then(function(response) {
         return response.json();
       })
       .then(function(response) {
-          console.log(response);
+          displayWeatherInfo(response);
+          
 });
+}
+
+function displayWeatherInfo(weatherData){
+    console.log(weatherData);
+    weatherData.clouds.all;
+    weatherData.main.humidity;
+    weatherData.main.pressure;
+    weatherData.main.temp;
+    weatherData.main.temp_max;
+    weatherData.main.temp_min;
+    //weatherData.mai
+    uvIndex(weatherData);
+
+
+document.querySelector('#cityHeading').innerHTML=weatherData.name;
+document.querySelector('#temp').innerHTML= weatherData.clouds.all;
+document.querySelector('#humidity').innerHTML=weatherData.main.humidity;
+document.querySelector("#pressureId").innerHTML= weatherData.main.pressure;
+
+    //display weather data 
+   // var weatherDisplayContainer = document.createElement("section");
+   // weatherDisplayContainer.classList.add("poop");
+    //append to container
+   // var weatherH1 =document.createElement("h1");
+    //weatherH1.innerHTML= weatherData.name;
+   // weatherDisplayContainer.appendChild(weatherH1);
+    
+
+    //append to Dom
+   // document.body.appendChild(weatherDisplayContainer);
+
 }
 
 var formSubmitHandler = function(event){
     event.preventDefault();
-    //var cityName = cityNameEl.value.trim();
-
-   var city = document.getElementById('cityName').value;
+    var cityName = cityNameEl.value.trim();
+    
+  // var city = document.getElementById('cityName').value;
 
     //check if City name is valid
-    if(city){
-        getWeatherInfo(city);
+    if(cityName){
+        
+        getWeatherInfo(cityName);
         cityNameEl.value= "";
+        
     }else{
         alert("Please enter a valid City Name");
     }
+    
 }
-
+//for search history part 
 var displayCities = function(){
     //clear old content
     cityContainerEl.textContent = "";
@@ -69,7 +105,7 @@ var displayCities = function(){
     for (var i=0;i<city.length; i++){
         //format cityName
 
-        // create a conteiner for each repo
+        // create a container for each repo
 
         //create a span element to hold city name
 
@@ -82,9 +118,55 @@ var displayCities = function(){
 
 }
 
-var uvIndex = function(){
+var uvIndex = function(weatherData){
+    fetch("https://api.openweathermap.org/data/2.5/onecall?lat="
+    + weatherData.coord.lat
+    +"&lon="
+    + weatherData.coord.lon
+    +"&appid="
+    +APIKey
+    + "&units=imperial"
+    )
+    .then(function(response) {
+        return response.json();
+      })
+      .then(function(response) {
+          uvData(response);
+          console.log(response);
+          
+});
+}
+
+function uvData(uvInfo){
+    var uvDisplayContainer = document.createElement("div");
+    console.log(uvInfo);
+    console.log(uvInfo.current.uvi);
+
+    //check UVIndex
+     //favorable under 3    
+    //moderate 3-5
+    //severe over 5 
+    if(uvInfo.current.uvi < 3){
+    uvDisplayContainer.classList.add("uvFavorable"); 
+    }
+    else if(uvInfo.current.uvi > 3 && uvInfo.current.uvi <=5 ){
+    uvDisplayContainer.classList.add("uvModerate"); 
+    }
+    else{
+        uvDisplayContainer.classList.add("uvSevere");
+    }
+    uvDisplayContainer.innerHTML= "<span>UV Index "+ uvInfo.current.uvi + "</span>";
+    document.body.appendChild(uvDisplayContainer)
     
-};
+//add class in CSS for favorable moderate and Severe
+
+}
+//5 day Forecast
+//use the daily One call, do the append to HTML 
+
+
+
+
+
 //add Listener for the form for city
 userSearchEl.addEventListener("submit", formSubmitHandler);
-getWeatherInfo();
